392ab7d8a00a2bedda7aa91b7260fd39cde862c5
abdullah-khanji <abdullah.us1721@gmail.com>
2024-07-04 00:35:36 +0500

    trained on 10epochs

--- 336_yolo_train.py
+++ 336_yolo_train.py
@@ -1,26 +1,25 @@
-from ultralytics import YOLO
-from matplotlib import pyplot as plt
-from PIL import Image
 import yaml
+from ultralytics import YOLO  # Assuming you are using the ultralytics YOLO library
+# import tensorflow as tf
 
-model= YOLO('yolov8n-seg.yaml')
-model= YOLO('yolov8n-seg.pt') #weights
+# Load YOLO model
+model = YOLO('yolov8n-seg.yaml')
+model = YOLO('yolov8n-seg.pt')
 
-with open('yolo_dataset/data.yaml') as stream:
-    num_classes=str(yaml.safe_load(stream)['nc'])
-    
-    
-project= '/results'
+# Load classes from data.yaml
+with open('yolo_dataset/data.yaml') as f:
+    classes = (yaml.safe_load(f)['nc'])
 
-name="3_epochs-"
+project = 'results/'
+name = '10_epochs-'
 
-
-results= model.train(
-    data= 'yolo_dataset/data.yaml',
+# Train the model
+results = model.train(
+    data='yolo_dataset/data.yaml',
     name=name,
     project=project,
-    epochs=3, 
+    epochs=10,
     patience=0,
-    batch=4,
+    batch=16,
     imgsz=512
 )
